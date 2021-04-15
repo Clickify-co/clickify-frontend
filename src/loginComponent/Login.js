6c163dacdf566c4dc39df333de5f2c82872be8da
Ashutosh Kumar <raj.fungus@gmail.com>
2021-04-15 19:53:12 +0530

    3PAges Done

--- src/loginComponent/Login.js
+++ src/loginComponent/Login.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import { Link } from '@reach/router'
+import Logo from '../clickify.png'
+import './login.css'
 
 class Login extends Component {
     render() {
         return (
             <div>
-                Login Page
+                <div className="center">
+                    <div className="logo-container">
+                        <img src={Logo} height='50px' alt="" />
+                    </div>
+                    <div className="form-box">
+                        <div className="heading-form">
+                            <h1>Login and Start generating leads</h1>
+                            <div className="text-gray">
+                                Do not have an account? <Link to='/register'>Sign up</Link>
+                            </div>
+                        </div>
+                        <div className="body-form">
+                            <span htmlFor="username" className="text-gray text-left">Enter Username</span>
+                            <input type="text" placeholder='Username' />
+                            <br />
+                            <span htmlFor="username" className="text-gray text-left">Enter Password</span>
+                            <input type="password" placeholder='Password' />
+                            <br />
+                            <Link to='/register' className='text-left'>Forgot Password?</Link>
+                            <input type="button" value="Sign in" />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/registerComponent/Register.js
+++ src/registerComponent/Register.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import Logo from '../clickify.png';
+import { Link } from '@reach/router'
 
 class Register extends Component {
     render() {
         return (
             <div>
-                Register Page
+                <div className="center">
+                    <div className="logo-container">
+                        <img src={Logo} height='50px' alt="" />
+                    </div>
+                    <div className="form-box">
+                        <div className="heading-form">
+                            <h1>Sign up and start shortening</h1>
+                            <div className="text-gray">
+                                Already a user? <Link to='/login'>Sign in</Link>
+                            </div>
+                        </div>
+                        <div className="body-form">
+                            <span htmlFor="username" className="text-gray text-left">Enter Username</span>
+                            <input type="text" placeholder='Username' />
+                            <br />
+                            <span htmlFor="email" className="text-gray text-left">Enter Email Address</span>
+                            <input type="text" placeholder='Email' />
+                            <br />
+                            <span htmlFor="username" className="text-gray text-left">Enter Password</span>
+                            <input type="password" placeholder='Password' />
+                            <br />
+                            <input type="button" value="Sign up" />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
