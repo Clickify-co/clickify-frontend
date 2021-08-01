c2db32c7353a1412434e958d3625c18a8344ebb3
Anshika Gupta <tanyagupta391@gmail.com>
2021-08-01 19:52:21 +0530

    Dashboard Table

--- src/dashboardComponent/Dashboard.js
+++ src/dashboardComponent/Dashboard.js
@@ -1,11 +1,49 @@
 import { Component } from "react";
 import './dashboard.css'
 import logo from '../clickify.png'
+import axios from "axios";
 class Dashboard extends Component{
     constructor(props){
-        super()
+        super(props)
+        this.state = {
+            fullURL : '',
+            urls: []
+        }
     }
 
+    componentDidMount(){
+        axios.get('http://localhost:3001/dashboard/',{headers:localStorage.getItem('auth_token')})
+        .then(response=>{
+            if(response.data.done){
+                console.log(response.data)
+                this.setState({urls: response.data.userData.urls})
+                
+
+            }
+            else{
+                alert('error Loading URLS')
+                console.log(response.data)
+            }
+        })
+    }
+
+    loadURL = ()=>{
+        
+    }
+
+    shortenURL(){
+        axios.post('http://localhost:3001/dashboard/addNewShortUrl',{
+            fullURL:this.state.fullURL
+        },{headers:{'auth-token': localStorage.getItem('auth_token')}})
+        .then(response=>{
+            if(response.data.done){
+                alert('ADDED')
+            }
+            else{
+                alert('SOme Error While Adding')
+            }
+        })
+    }
 
    render(){
        return(
@@ -23,9 +61,11 @@ class Dashboard extends Component{
             Dashboard
         </div>
         <div className="input-fields">
-            <input type="text" placeholder="Enter a title for short link"/>
-            <input type="text" placeholder="Enter full Url to shrink"/>
-            <button className="btn">Shorten</button>
+            <input type="text"
+                value={this.state.fullURL}
+                onChange={(e)=>{this.setState({fullURL: e.target.value})}}
+             placeholder="Enter full Url to shrink"/>
+            <button className="btn" onClick={()=>{this.shortenURL()}}>Shorten</button>
         </div>
         <div className="table">
             <div className="heading">
@@ -36,7 +76,6 @@ class Dashboard extends Component{
           <table cellPadding={0} cellSpacing={0} border={0}>
             <thead>
               <tr>
-                <th>Title</th>
                 <th>Full URL</th>
                 <th>ShortURL</th>
                 <th>Custom Back Part</th>
@@ -48,13 +87,16 @@ class Dashboard extends Component{
         <div className="tbl-content">
           <table cellPadding={0} cellSpacing={0} border={0}>
             <tbody>
-              <tr>
-                <td>AAC</td>
-                <td>AUSTRALIAN COMPANY </td>
-                <td>$1.38</td>
-                <td>+2.01</td>
-                <td>-0.36%</td>
-              </tr>
+              {this.state.urls?this.state.urls.map(url=>{
+                  return (<tr>
+                      <td>
+                          {url.fullURL}
+                      </td>
+                      <td>{url.shortURL}</td>
+                      <td>{url.customBackPart?url.customBackPart:'No Custom Back Part'}</td>
+                      <td></td>
+                  </tr>)
+              }):'Loading'}
             </tbody>
           </table>
         </div>
